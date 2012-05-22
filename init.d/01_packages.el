243f3609d1d7eb78da0d1dfb9ee1b7a30a8f0f54
Ted Kulp <ted@tedkulp.com>
2012-05-21 20:21:38 -0400

    Initial Commit

---
+++ init.d/01_packages.el
@@ -0,0 +1,20 @@
+;; My package.el repos
+(require 'package)
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-ruby ace-jump-mode color-theme-sanityinc-solarized)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
---
+++ init.el
@@ -0,0 +1,13 @@
+(setq cust-emacs-init-file load-file-name)
+(setq cust-emacs-config-dir
+      (file-name-directory cust-emacs-init-file))
+(setq cust-init-dir
+      (expand-file-name "init.d" cust-emacs-config-dir))
+
+(if (file-exists-p cust-init-dir)
+    (dolist (file (directory-files cust-init-dir t "\.el$"))
+        (load file)))
+
+;; Set up 'custom' system
+(setq custom-file (expand-file-name "emacs-customizations.el" cust-emacs-config-dir))
+(load custom-file)
